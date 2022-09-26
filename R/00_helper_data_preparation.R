f0d97f19847df1a705066dec58b736ac30bd0329
IlyaZar <zarubin@wiso.uni-koeln.de>
2022-09-26 16:34:01 +0200

    Major refactoring
    - encapsulate data cleaning into separate functions:
      (00_helper_data_preparation)
    - improve perform_tests: makes it easier to perform tests;
      avoid data pre-cleaning and preparation
    - update documentation

--- R/00_helper_data_preparation.R
+++ R/00_helper_data_preparation.R
@@ -83,26 +83,31 @@ get_list_test_week <- function(pth,
 #' those need adjustments. The list summarizes the output for each week as
 #' returned from [get_list_test_week()].
 #'
-#' @param pth path to all data sets
+#' @param pth path to data set that stores the plan of campaigns
+#' @param list_data_validate a list of data sets to validate product IDs against
+#'    plan of campaigns
 #'
 #' @return a list of lists, with element of the latter being output from
 #'   [get_list_test_week()].
 #' @export
-get_meta_data <- function(pth, data_validate) {
-  product_ids_avail  <- names(data_validate)
+get_meta_data <- function(pth, list_data_validate) {
+  num_vecka <- length(list_data_validate)
   names_sheets_vecka <- names(xlsx::getSheets(xlsx::loadWorkbook(pth)))
-  names_sheets_vecka <- names_sheets_vecka[[1]]
-  num_sheets_vecka   <- length(names_sheets_vecka)
+  names_sheets_vecka <- names_sheets_vecka[1:num_vecka]
 
-  out_list_test_weeks <- vector("list", num_sheets_vecka)
-  for (i in seq_len(num_sheets_vecka)) {
+  product_ids_avail_list <- vector("list", num_vecka)
+  for (i in 1:num_vecka) {
+    product_ids_avail_list[[i]] <- names(list_data_validate[[i]])
+  }
+  out_list_test_weeks <- vector("list", num_vecka)
+  for (i in seq_len(num_vecka)) {
     out_list_test_weeks[[i]] <- get_list_test_week(pth, names_sheets_vecka[i],
-                                                   product_ids_avail)
+                                                   product_ids_avail_list[[i]])
   }
   names(out_list_test_weeks) <- names_sheets_vecka
   return(out_list_test_weeks)
 }
-#' Generates merged data sets on sales.
+#' Generates merged data set on sales.
 #'
 #' Uses first argument to get information on sales (count) of relevant products
 #' and second data set on counts of all products sold, and both to compute
@@ -120,105 +125,24 @@ get_meta_data <- function(pth, data_validate) {
 #'   of relevant products per zoner, kampanj and skylttyp or 'daily' for daily
 #'   counts and frequencies
 #'
-#' @return a list of all data sets computed as in the Description
+#' @return the data set computed as outlined in the description
 #' @export
 get_merged_data_set <- function(data_sales1,
                                 data_sales2,
                                 list_test_weeks,
                                 time_frq = "weekly") {
-  data_sets     <- vector("list", 2)
-  if (time_frq == "daily") {
-    data_sets[[1]] <- data_sales1 %>%
-      dplyr::group_by(.data$butik, .data$vecka, .data$datum) %>%
-      dplyr::summarize_if(.predicate = is.numeric,.funs = sum, na.rm = TRUE)
 
-    data_sets[[2]] <- data_sales2 %>%
-      dplyr::select(-.data$timme) %>%
-      dplyr::group_by(.data$butik, .data$vecka, .data$datum) %>%
-      dplyr::summarize_if(.predicate = is.numeric,.funs = sum, na.rm = TRUE)
+  data_sets_list <- get_data_sets_list(data_sales1 = data_sales1,
+                                       data_sales2 = data_sales2,
+                                       time_frq = time_frq)
+  data_final <- data_sets_list$data_final
+  col_names_removed <- data_sets_list$col_names_removed
 
-    product_ids <- setdiff(names(data_sets[[1]]),
-                           c("butik", "vecka", "datum"))
-    col_names_removed <- product_ids
+  data_final <- data_final %>%
+    add_zoner_vars(tmp_list = list_test_weeks) %>%
+    add_skylttyp_vars(tmp_list = list_test_weeks) %>%
+    add_kampanj_vars(tmp_list = list_test_weeks)
 
-  } else if (time_frq == "weekly") {
-    data_sets[[1]] <- data_sales1 %>%
-      dplyr::select(-.data$datum) %>%
-      dplyr::group_by(.data$butik, .data$vecka) %>%
-      dplyr::summarize_if(.predicate = is.numeric,.funs = sum, na.rm = TRUE)
-
-    data_sets[[2]] <- data_sales2 %>%
-      dplyr::select(-.data$datum) %>%
-      dplyr::select(-.data$timme) %>%
-      dplyr::group_by(.data$butik, .data$vecka) %>%
-      dplyr::summarize_if(.predicate = is.numeric,.funs = sum, na.rm = TRUE)
-
-    product_ids <- setdiff(names(data_sets[[1]]),
-                           c("butik", "vecka"))
-    col_names_removed <- product_ids
-  } else {
-    msg <- paste0("Unrecognized argument value to 'time_frq': ",
-                  "either daily or weekly. ")
-    stop(msg)
-  }
-
-  data_final <- dplyr::inner_join(data_sets[[1]], data_sets[[2]])
-
-  names_vecka_testing <- names(list_test_weeks)
-  num_vecka_testing   <- length(names_vecka_testing)
-  out_data_sets       <- vector("list", num_vecka_testing)
-
-  for (i in 1:num_vecka_testing) {
-    tmp_list <- list_test_weeks[[i]]
-
-    names_zoner <- names(tmp_list[["zoner"]])
-    num_zoner   <- length(names_zoner)
-    for(j in 1:num_zoner) {
-      tmp_prod_ids <- as.character(tmp_list[["zoner"]][[j]])
-      tmp_name1    <- paste0("sales_zoner_count_", names_zoner[j])
-      tmp_name2    <- paste0("sales_zoner_frac_", names_zoner[j])
-      data_final <- data_final %>%
-        dplyr::mutate({{ tmp_name1 }} := rowSums(dplyr::across(tidyselect::all_of(tmp_prod_ids)),
-                                                 na.rm = TRUE)) %>%
-        dplyr::mutate({{ tmp_name2 }} := .data[[tmp_name1]] / .data$antal_kvitton)
-    }
-    data_final <- data_final %>%
-      dplyr::mutate(sales_zoner_count_TOTAL = rowSums(dplyr::across(dplyr::contains("sales_zoner_count_"),
-                                                      na.rm = TRUE))) %>%
-      dplyr::mutate(sales_zoner_frac_TOTAL = .data$sales_zoner_count_TOTAL / .data$antal_kvitton)
-
-    names_skylttyp <- names(tmp_list[["skylttyp"]])
-    num_skylttyp   <- length(names_skylttyp)
-    for(j in 1:num_skylttyp) {
-      tmp_prod_ids <- as.character(tmp_list[["skylttyp"]][[j]])
-      tmp_name1    <- paste0("sales_skylttyp_count_", names_skylttyp[j])
-      tmp_name2    <- paste0("sales_skylttyp_frac_", names_skylttyp[j])
-      data_final <- data_final %>%
-        dplyr::mutate({{ tmp_name1 }} := rowSums(dplyr::across(tidyselect::all_of(tmp_prod_ids)),
-                                                 na.rm = TRUE)) %>%
-        dplyr::mutate({{ tmp_name2 }} := .data[[tmp_name1]] / .data$antal_kvitton)
-    }
-    data_final <- data_final %>%
-      dplyr::mutate(sales_skylttyp_count_TOTAL = rowSums(dplyr::across(dplyr::contains("sales_skylttyp_count_"),
-                                                                      na.rm = TRUE))) %>%
-      dplyr::mutate(sales_skylttyp_frac_TOTAL = .data$sales_skylttyp_count_TOTAL / .data$antal_kvitton)
-
-    names_kampanj <- names(tmp_list[["kampanj"]])
-    num_kampanj   <- length(names_kampanj)
-    for(j in 1:num_kampanj) {
-      tmp_prod_ids <- as.character(tmp_list[["kampanj"]][[j]])
-      tmp_name1    <- paste0("sales_kampanj_count_", names_kampanj[j])
-      tmp_name2    <- paste0("sales_kampanj_frac_", names_kampanj[j])
-      data_final <- data_final %>%
-        dplyr::mutate({{ tmp_name1 }} := rowSums(dplyr::across(tidyselect::all_of(tmp_prod_ids)),
-                                                 na.rm = TRUE)) %>%
-        dplyr::mutate({{ tmp_name2 }} := .data[[tmp_name1]] / .data$antal_kvitton)
-    }
-    data_final <- data_final %>%
-      dplyr::mutate(sales_kampanj_count_TOTAL = rowSums(dplyr::across(dplyr::contains("sales_kampanj_count_"),
-                                                                      na.rm = TRUE))) %>%
-      dplyr::mutate(sales_kampanj_frac_TOTAL = .data$sales_kampanj_count_TOTAL / .data$antal_kvitton)
-  }
   data_out <- data_final[, which(!(names(data_final) %in% col_names_removed))]
   if (time_frq == "daily") {
     data_out <- data_out %>% dplyr::arrange(dplyr::across(c("butik",
@@ -258,6 +182,101 @@ get_merged_data_set <- function(data_sales1,
                                            dplyr::contains("sales_skylttyp"))
   }
   return(data_out)
+}
+get_data_sets_list <- function(data_sales1, data_sales2, time_frq) {
+    data_sets     <- vector("list", 2)
+  if (time_frq == "daily") {
+    data_sets[[1]] <- data_sales1 %>%
+      dplyr::group_by(.data$butik, .data$vecka, .data$datum) %>%
+      dplyr::summarize_if(.predicate = is.numeric,.funs = sum, na.rm = TRUE)
+
+    data_sets[[2]] <- data_sales2 %>%
+      dplyr::select(-.data$timme) %>%
+      dplyr::group_by(.data$butik, .data$vecka, .data$datum) %>%
+      dplyr::summarize_if(.predicate = is.numeric,.funs = sum, na.rm = TRUE)
+
+    product_ids <- setdiff(names(data_sets[[1]]),
+                           c("butik", "vecka", "datum"))
+    col_names_removed <- product_ids
+
+  } else if (time_frq == "weekly") {
+    data_sets[[1]] <- data_sales1 %>%
+      dplyr::select(-.data$datum) %>%
+      dplyr::group_by(.data$butik, .data$vecka) %>%
+      dplyr::summarize_if(.predicate = is.numeric,.funs = sum, na.rm = TRUE)
+
+    data_sets[[2]] <- data_sales2 %>%
+      dplyr::select(-.data$datum) %>%
+      dplyr::select(-.data$timme) %>%
+      dplyr::group_by(.data$butik, .data$vecka) %>%
+      dplyr::summarize_if(.predicate = is.numeric,.funs = sum, na.rm = TRUE)
+
+    product_ids <- setdiff(names(data_sets[[1]]),
+                           c("butik", "vecka"))
+    col_names_removed <- product_ids
+  } else {
+    msg <- paste0("Unrecognized argument value to 'time_frq': ",
+                  "either daily or weekly. ")
+    stop(msg)
+  }
+
+  data_final <- dplyr::inner_join(data_sets[[1]], data_sets[[2]])
+  return(list(data_final = data_final, col_names_removed = col_names_removed))
+}
+add_zoner_vars <- function(data_set, tmp_list) {
+  names_zoner <- names(tmp_list[["zoner"]])
+  num_zoner   <- length(names_zoner)
+  for(j in 1:num_zoner) {
+    tmp_prod_ids <- as.character(tmp_list[["zoner"]][[j]])
+    tmp_name1    <- paste0("sales_zoner_count_", names_zoner[j])
+    tmp_name2    <- paste0("sales_zoner_frac_", names_zoner[j])
+    data_set <- data_set %>%
+      dplyr::mutate({{ tmp_name1 }} := rowSums(dplyr::across(tidyselect::all_of(tmp_prod_ids)),
+                                               na.rm = TRUE)) %>%
+      dplyr::mutate({{ tmp_name2 }} := .data[[tmp_name1]] / .data$antal_kvitton)
+  }
+  data_set <- data_set %>%
+    dplyr::mutate(sales_zoner_count_TOTAL = rowSums(dplyr::across(dplyr::contains("sales_zoner_count_"),
+                                                                  na.rm = TRUE))) %>%
+    dplyr::mutate(sales_zoner_frac_TOTAL = .data$sales_zoner_count_TOTAL / .data$antal_kvitton)
+  data_set
+}
+add_skylttyp_vars <- function(data_set, tmp_list) {
+  names_skylttyp <- names(tmp_list[["skylttyp"]])
+  num_skylttyp   <- length(names_skylttyp)
+  for(j in 1:num_skylttyp) {
+    tmp_prod_ids <- as.character(tmp_list[["skylttyp"]][[j]])
+    tmp_name1    <- paste0("sales_skylttyp_count_", names_skylttyp[j])
+    tmp_name2    <- paste0("sales_skylttyp_frac_", names_skylttyp[j])
+    data_set <- data_set %>%
+      dplyr::mutate({{ tmp_name1 }} := rowSums(dplyr::across(tidyselect::all_of(tmp_prod_ids)),
+                                               na.rm = TRUE)) %>%
+      dplyr::mutate({{ tmp_name2 }} := .data[[tmp_name1]] / .data$antal_kvitton)
+  }
+  data_set <- data_set %>%
+    dplyr::mutate(sales_skylttyp_count_TOTAL = rowSums(dplyr::across(dplyr::contains("sales_skylttyp_count_"),
+                                                                     na.rm = TRUE))) %>%
+    dplyr::mutate(sales_skylttyp_frac_TOTAL = .data$sales_skylttyp_count_TOTAL / .data$antal_kvitton)
+  data_set
+}
+add_kampanj_vars <- function(data_set,
+                             tmp_list) {
+  names_kampanj <- names(tmp_list[["kampanj"]])
+  num_kampanj   <- length(names_kampanj)
+  for(j in 1:num_kampanj) {
+    tmp_prod_ids <- as.character(tmp_list[["kampanj"]][[j]])
+    tmp_name1    <- paste0("sales_kampanj_count_", names_kampanj[j])
+    tmp_name2    <- paste0("sales_kampanj_frac_", names_kampanj[j])
+    data_set <- data_set %>%
+      dplyr::mutate({{ tmp_name1 }} := rowSums(dplyr::across(tidyselect::all_of(tmp_prod_ids)),
+                                               na.rm = TRUE)) %>%
+      dplyr::mutate({{ tmp_name2 }} := .data[[tmp_name1]] / .data$antal_kvitton)
+  }
+  data_set <- data_set %>%
+    dplyr::mutate(sales_kampanj_count_TOTAL = rowSums(dplyr::across(dplyr::contains("sales_kampanj_count_"),
+                                                                    na.rm = TRUE))) %>%
+    dplyr::mutate(sales_kampanj_frac_TOTAL = .data$sales_kampanj_count_TOTAL / .data$antal_kvitton)
+  data_set
 }
 #' Generate data sets per butik/shop, week, and reference unit.
 #'
--- R/01_hypothesis_tests.R
+++ R/01_hypothesis_tests.R
@@ -1,61 +1,49 @@
 #' Performs various hypothesis tests on data.
 #'
 #' Hypothesis tests performed consider the different proportions of sales
-#' of advertised products between specified control period (\code{cntr_period})
-#' and testing period (\code{test_period}), the latter being characterized by
+#' of advertised products between specified control group (\code{cntrl})
+#' and testing group (\code{test}), the latter being characterized by
 #' the placement of screens to enhance consumer demand and increase sales.
 #'
 #' @param data_set the data set for a specific butik/shop
-#' @param ref_unit a character specifying the reference unit of either
-#'   \itemize{
-#'     \item "zoner"
-#'     \item "kampanj"
-#'     \item "skylttyp"
-#'   }
-#'   for which the hypothesis test should be performed. Additionally, total
-#'   sales i.e. the overall comparison for a shop/butik between testing and
-#'   control phase is performed. The number of hypothesis tests equals therefor
-#'   the number of different values for the reference unit + 1 (the overall
-#'   test).
-#' @param cntr_period an integer sequence (from to e.g. \code{28:33}) specifying
-#'   the weeks of the control period
-#' @param test_period an integer sequence (from to e.g. \code{34:35}) specifying
-#'   the weeks of the testing period
 #'
-#' @return a list of hypothesis outputs of dimension \code{length(ref_unit) + 1}
-#'   with each element containing the corresponding tests performed (Fisher, ..)
+#' @param type type of discriminating variable to fix test vs. control groups:
+#'   either vecka (comparing groups) or butik (comparing stores) against each
+#'   other
+#' @param cntrl an integer sequence (from to e.g. \code{28:33}) specifying
+#'   the weeks of the control group or a character sequence specifying the butik
+#'   names of the control group
+#' @param test an integer sequence (from to e.g. \code{34:35}) specifying
+#'   the weeks of the testing group or a character sequence specifying the butik
+#'   names of the testing group
+#'
+#' @return a list of hypothesis-test outputs of dimension
+#'   \code{length(ref_unit) + 1} with each element containing the corresponding
+#'   tests performed (Fisher, ..)
 #' @export
 perform_tests <- function(data_set,
-                          ref_unit,
-                          cntr_period,
-                          test_period) {
-  ref_unit2 <- paste0("sales_", ref_unit, "_count")
-  cntr_period_full <- as.numeric(paste0("2022", cntr_period))
-  test_period_full <- as.numeric(paste0("2022", test_period))
-  all_periods_char <- c(cntr_period_full, test_period_full)
-  tmp_data <- data_set %>%
-    dplyr::filter(.data$vecka %in% all_periods_char) %>%
-    dplyr::select(.data$vecka, .data$antal_kvitton, dplyr::contains(ref_unit2))
-
-  names_subtests <- setdiff(names(tmp_data), c("vecka",
-                                               "butik",
-                                               "antal_kvitton"))
+                          type = NULL,
+                          cntrl,
+                          test) {
+  names_subtests <- setdiff(names(data_set),
+                            c("butik", "vecka", "datum","antal_kvitton"))
   num_subtests <- length(names_subtests)
   names_tests <- c("fisher", "Boschlo")
   out_test_results <- vector("list", num_subtests)
   for (i in 1:num_subtests) {
-    out_test_results[[i]] <- test_fisher(tmp_data,
+    out_test_results[[i]] <- test_fisher(data_set,
                                          var_to_test = names_subtests[i],
-                                         cntr_period = cntr_period_full,
-                                         test_period = test_period_full)
+                                         type = type,
+                                         cntrl = cntrl,
+                                         test = test)
     # out_test_results[[i]][[1]] <- test_fisher(tmp_data,
     #                                      var_to_test = names_subtests[i],
-    #                                      cntr_period = cntr_period_full,
-    #                                      test_period = test_period_full)
+    #                                      cntrl = cntrl_full,
+    #                                      test = test_full)
     # out_test_results[[i]][[2]] <- OTHER TEST(tmp_data,
     #                                      var_to_test = names_subtests[i],
-    #                                      cntr_period = cntr_period_full,
-    #                                      test_period = test_period_full)
+    #                                      cntrl = cntrl_full,
+    #                                      test = test_full)
     # names(out_test_results[[i]]) <- names_tests
   }
   names(out_test_results) <- names_subtests
@@ -64,13 +52,16 @@ perform_tests <- function(data_set,
 #' Performs the exact Fisher test for the specific subset of data.
 #'
 #' @param data_sub_set a data set as passed via \code{data_set} from
-#'    [perform_tests()] but additionally filtered for testing and control period
+#'    [perform_tests()] but additionally filtered for testing and controlgroup
 #' @inheritParams perform_tests
 #'
 #' @return test matrix for, and output matrix from [fisher.test()] for specific
 #'    data subset
-test_fisher <- function(data_sub_set, var_to_test, cntr_period, test_period,
-                         options = NULL) {
+test_fisher <- function(data_sub_set,
+                        var_to_test,
+                        type,
+                        cntrl, test,
+                        options = NULL) {
   if (!is.null(options())) {
     conf_lvl <- 0.95
     alternative <- "two.sided"
@@ -81,8 +72,9 @@ test_fisher <- function(data_sub_set, var_to_test, cntr_period, test_period,
 
   test_fisher_mat <- get_matrix_fisher(data_sub_set,
                                        var_to_test,
-                                       cntr_period,
-                                       test_period)
+                                       type,
+                                       cntrl,
+                                       test)
   test_fisher <- fisher.test(test_fisher_mat,
                              alternative = alternative,
                              conf.level = conf_lvl)
@@ -98,29 +90,40 @@ test_fisher <- function(data_sub_set, var_to_test, cntr_period, test_period,
 #'   \code{dimnames}
 get_matrix_fisher <- function(data_sub_set,
                               var_to_test,
-                              cntr_period,
-                              test_period) {
-  output_dimnames <- list(c("testing weeks",
-                            "control weeks"),
-                          c("sales target product",
-                            "sales other products"))
+                              type,
+                              cntrl,
+                              test) {
+  stopifnot(`unknown arg-value for 'type'` = any(type %in% c("vecka", "butik")))
+  if (type == "vecka") {
+    output_dimnames <- list(c("testing weeks",
+                              "control weeks"),
+                            c("sales target product",
+                              "sales other products"))
+
+  } else {
+    output_dimnames <- list(c("testing stores",
+                              "control stores"),
+                            c("sales target product",
+                              "sales other products"))
+  }
+
   total_sales_cntr <- data_sub_set %>%
-    dplyr::filter(.data$vecka %in% cntr_period) %>%
+    dplyr::filter(.data[[type]] %in% cntrl) %>%
     dplyr::pull(.data$`antal_kvitton`)%>%
     round(digits = 0)
 
   total_sales_test <- data_sub_set %>%
-    dplyr::filter(.data$vecka %in% test_period) %>%
+    dplyr::filter(.data[[type]] %in% test) %>%
     dplyr::pull(.data$`antal_kvitton`)%>%
     round(digits = 0)
 
   target_sales_cntr <- data_sub_set %>%
-    dplyr::filter(.data$vecka %in% cntr_period) %>%
+    dplyr::filter(.data[[type]] %in% cntrl) %>%
     dplyr::pull(var_to_test) %>%
     round(digits = 0)
 
   target_sales_test <- data_sub_set %>%
-    dplyr::filter(.data$vecka %in% test_period) %>%
+    dplyr::filter(.data[[type]] %in% test) %>%
     dplyr::pull(var_to_test)%>%
     round(digits = 0)
 
