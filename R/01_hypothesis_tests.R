bb37f6f8565d4c1753b6efb4914bf44e0fa7335e
IlyaZar <zarubin@wiso.uni-koeln.de>
2022-09-29 14:37:00 +0200

    Refactor hypothesis test functions
    - add prop.test()
    - allow to pass significance levels via options list
    - allow to pass alternative hypothesis specifications via options list
    - clean documentation

--- R/01_hypothesis_tests.R
+++ R/01_hypothesis_tests.R
@@ -1,52 +1,54 @@
 #' Performs various hypothesis tests on data.
 #'
 #' Hypothesis tests performed consider the different proportions of sales
-#' of advertised products between specified control group (\code{cntrl})
-#' and testing group (\code{test}), the latter being characterized by
+#' of advertised products between specified control group (\code{cntrl_group})
+#' and testing group (\code{test_group}), the latter being characterized by
 #' the placement of screens to enhance consumer demand and increase sales.
 #'
 #' @param data_set the data set for a specific butik/shop
-#'
-#' @param type type of discriminating variable to fix test vs. control groups:
-#'   either vecka (comparing groups) or butik (comparing stores) against each
-#'   other
-#' @param cntrl an integer sequence (from to e.g. \code{28:33}) specifying
+#' @param type_test character value identifying the type of test to perform
+#' @param type_group_var type_group_var of discriminating variable to fix test vs.
+#'   control groups: either vecka (comparing groups) or butik (comparing stores)
+#'   against each other
+#' @param cntrl_group an integer sequence (from to e.g. \code{28:33}) specifying
 #'   the weeks of the control group or a character sequence specifying the butik
 #'   names of the control group
-#' @param test an integer sequence (from to e.g. \code{34:35}) specifying
-#'   the weeks of the testing group or a character sequence specifying the butik
-#'   names of the testing group
+#' @param test_group an integer sequence (from to e.g. \code{34:35}) specifying
+#'   the weeks of the testing group (if \code{type_group} == "vecka") or
+#'   (if \code{type_group} == "butik"), a character sequence specifying the
+#'   butik names of the testing group
+#' @param options options passed to test function as a list of two elements:
+#'    \itemize{
+#'    \item \code{conf_level}: defaults to 0.95 and within range (0,1)
+#'    \item \code{alternative}: defaults to "two.sided" with other options
+#'       being "greater" or "less"
+#'    }
 #'
-#' @return a list of hypothesis-test outputs of dimension
-#'   \code{length(ref_unit) + 1} with each element containing the corresponding
-#'   tests performed (Fisher, ..)
+#' @return a list of hypothesis-test outputs with each element being a test for
+#'   a sub-sample of the data (see internals for details) that contains the
+#'   test-output (which varies depending on \code{type_test})
 #' @export
 perform_tests <- function(data_set,
-                          type = NULL,
-                          cntrl,
-                          test) {
-  names_subtests <- setdiff(names(data_set),
-                            c("butik", "vecka", "datum","antal_kvitton"))
-  num_subtests <- length(names_subtests)
-  names_tests <- c("fisher", "Boschlo")
+                          type_test,
+                          type_group_var = NULL,
+                          cntrl_group,
+                          test_group,
+                          options) {
+  names_data_subsets <- setdiff(names(data_set),
+                                c("butik", "vecka", "datum","antal_kvitton"))
+  num_subtests <- length(names_data_subsets)
+
   out_test_results <- vector("list", num_subtests)
   for (i in 1:num_subtests) {
-    out_test_results[[i]] <- test_fisher(data_set,
-                                         var_to_test = names_subtests[i],
-                                         type = type,
-                                         cntrl = cntrl,
-                                         test = test)
-    # out_test_results[[i]][[1]] <- test_fisher(tmp_data,
-    #                                      var_to_test = names_subtests[i],
-    #                                      cntrl = cntrl_full,
-    #                                      test = test_full)
-    # out_test_results[[i]][[2]] <- OTHER TEST(tmp_data,
-    #                                      var_to_test = names_subtests[i],
-    #                                      cntrl = cntrl_full,
-    #                                      test = test_full)
-    # names(out_test_results[[i]]) <- names_tests
+    out_test_results[[i]] <- perform_test(data_set,
+                                          var_to_test = names_data_subsets[i],
+                                          type_test = type_test,
+                                          type_group_var = type_group_var,
+                                          cntrl_group = cntrl_group,
+                                          test_group = test_group,
+                                          options = options)
   }
-  names(out_test_results) <- names_subtests
+  names(out_test_results) <-names_data_subsets
   return(out_test_results)
 }
 #' Performs the exact Fisher test for the specific subset of data.
@@ -55,83 +57,93 @@ perform_tests <- function(data_set,
 #'    [perform_tests()] but additionally filtered for testing and controlgroup
 #' @inheritParams perform_tests
 #'
-#' @return test matrix for, and output matrix from [fisher.test()] for specific
-#'    data subset
-test_fisher <- function(data_sub_set,
-                        var_to_test,
-                        type,
-                        cntrl, test,
-                        options = NULL) {
-  if (!is.null(options())) {
-    conf_lvl <- 0.95
-    alternative <- "two.sided"
+#' @return data matrix for, and output matrix from different tests e.g.
+#'    [fisher.test()] for a single specific data (subset)
+perform_test <- function(data_sub_set,
+                         var_to_test,
+                         type_test,
+                         type_group_var,
+                         cntrl_group,
+                         test_group,
+                         options = list(conf_level = 0.95,
+                                        alternative = "two.sided")) {
+  if (is.null(options)) {
+    options <- list()
+    options$conf_level  <- 0.95
+    options$alternative <- "tow.sided"
+  }
+  test_matrix <- get_test_matrix(data_sub_set,
+                                 var_to_test,
+                                 type_group_var,
+                                 cntrl_group,
+                                 test_group)
+  if (type_test == "fisher") {
+    test_output <- fisher.test(x = test_matrix,
+                               alternative = options$alternative,
+                               conf.level = options$conf_level)
+  } else if (type_test == "proportional") {
+      test_output <- prop.test(x = t(test_matrix),
+                               n = NULL, # argument ignored as 'x' is a matrix
+                               alternative = options$alternative,
+                               conf.level = options$conf_level)
   } else {
-    conf_lvl <- options$confidence
-    alternative <- options$alternative
+    stop("Unknown val. to arg. 'type_test'; see help for supported values.")
   }
 
-  test_fisher_mat <- get_matrix_fisher(data_sub_set,
-                                       var_to_test,
-                                       type,
-                                       cntrl,
-                                       test)
-  test_fisher <- fisher.test(test_fisher_mat,
-                             alternative = alternative,
-                             conf.level = conf_lvl)
-  list(testmatrix = test_fisher_mat,
-       testoutput = test_fisher)
+  list(testmatrix = test_matrix,
+       testoutput = test_output)
 }
-
-#' Generates numeric matrix from data subset suitable for [fisher.test()].
+#' Generates numeric matrix from data subset suitable for different tests
 #'
-#' @inheritParams test_fisher
+#' Supported tests are [prop.test()], [fisher.test()], ...
 #'
-#' @return a matrix suitable for [fisher.test()] with appropriate
-#'   \code{dimnames}
-get_matrix_fisher <- function(data_sub_set,
-                              var_to_test,
-                              type,
-                              cntrl,
-                              test) {
-  stopifnot(`unknown arg-value for 'type'` = any(type %in% c("vecka", "butik")))
-  if (type == "vecka") {
-    output_dimnames <- list(c("testing weeks",
-                              "control weeks"),
-                            c("sales target product",
-                              "sales other products"))
+#' @inheritParams perform_test
+#'
+#' @return a matrix suitable as first argument to these tests (see Descripiton)
+get_test_matrix <- function(data_sub_set,
+                            var_to_test,
+                            type_group_var,
+                            cntrl_group,
+                            test_group) {
+  check_group_type <- any(type_group_var %in% c("vecka", "butik"))
+  stopifnot(`unknown arg-value for 'type_group_var'` = check_group_type)
+  if (type_group_var == "vecka") {
+    output_dimnames <- list(c("sales target product",
+                              "sales other products"),
+                            c("testing weeks",
+                              "control weeks"))
 
   } else {
-    output_dimnames <- list(c("testing stores",
-                              "control stores"),
-                            c("sales target product",
-                              "sales other products"))
+    output_dimnames <- list(c("sales target product",
+                              "sales other products"),
+                            c("testing stores",
+                              "control stores"))
   }
 
   total_sales_cntr <- data_sub_set %>%
-    dplyr::filter(.data[[type]] %in% cntrl) %>%
+    dplyr::filter(.data[[type_group_var]] %in% cntrl_group) %>%
     dplyr::pull(.data$`antal_kvitton`)%>%
     round(digits = 0)
 
   total_sales_test <- data_sub_set %>%
-    dplyr::filter(.data[[type]] %in% test) %>%
+    dplyr::filter(.data[[type_group_var]] %in% test_group) %>%
     dplyr::pull(.data$`antal_kvitton`)%>%
     round(digits = 0)
 
   target_sales_cntr <- data_sub_set %>%
-    dplyr::filter(.data[[type]] %in% cntrl) %>%
+    dplyr::filter(.data[[type_group_var]] %in% cntrl_group) %>%
     dplyr::pull(var_to_test) %>%
     round(digits = 0)
 
   target_sales_test <- data_sub_set %>%
-    dplyr::filter(.data[[type]] %in% test) %>%
+    dplyr::filter(.data[[type_group_var]] %in% test_group) %>%
     dplyr::pull(var_to_test)%>%
     round(digits = 0)
 
   out <- matrix(sapply(list(target_sales_test, total_sales_test,
-                           target_sales_cntr, total_sales_cntr), sum),
-                nrow = 2, ncol = 2,
-                byrow = TRUE)
-  out[, 2] <- out[, 2, drop = FALSE] - out[, 1, drop = FALSE]
+                            target_sales_cntr, total_sales_cntr), sum),
+                nrow = 2, ncol = 2)
+  out[2, ] <- out[2, , drop = FALSE] - out[1, , drop = FALSE]
   dimnames(out) <- output_dimnames
   return(out)
 }
